519d1e4d930d88895eae7d9e6110a7a33ed02026
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-18 19:36:04 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "cd97541f-358a-4ad9-bdc4-d267f62b84b9",
+    "home_feed": [
+        {
+            "id": "0f3d3355-879f-44d7-9fd3-0bd307d213c6",
+            "artist_name": "reason",
+            "home_carousel": [
+                {
+                    "id": "9219d6e0-92b8-43f8-ab3a-943eb1852c31",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "b70747a7-ee6e-40e2-bbe9-9f395d6fa6f0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "1139c51a-2de7-49b3-9435-1ec95ee968e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "b941e094-d759-4c1b-a461-f21ad3b76cfb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "ede5faaa-1923-4d31-a655-39a8dd9bcf5b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                }
+            ],
+            "art_caption": "good",
+            "art_description": "Add heavy find road oil look image. Southern worry evening participant current charge. Section know read support food consider team notice."
+        },
+        {
+            "id": "fd208b7e-db45-4043-99ea-22f6b1b329a7",
+            "artist_name": "visit",
+            "home_carousel": [
+                {
+                    "id": "3348828c-1ffe-4e85-9690-5bbb983ef447",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "ffc0a5c0-8554-4c23-aa50-f0657b82b1f6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "e4932950-5b4f-4501-8550-3e1cfc9aa2b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "60ccc527-d2c7-4bb0-a0ad-db0a14c72039",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "e95b6c32-fa5f-4a8a-a573-0a119639d13d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                }
+            ],
+            "art_caption": "bill",
+            "art_description": "South positive body money itself should. Determine meet dark process standard language."
+        },
+        {
+            "id": "4919f96a-80ff-4d9a-b7ab-c9803fa85629",
+            "artist_name": "drive",
+            "home_carousel": [
+                {
+                    "id": "fd3eaa69-a8c4-4b06-b851-292ccf37745a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "05ef9ee3-bef9-4cb8-941f-03078a628e12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "a71bde13-1fa7-4927-bf29-78b1c5406793",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "1103ed9b-726b-40ba-961e-24cc669bf7de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/102.png"
+                },
+                {
+                    "id": "f0fba0b7-e2c3-4819-80ed-be3583d6938d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                }
+            ],
+            "art_caption": "born",
+            "art_description": "Clear might develop walk. Toward throughout network to fight song fast. Suggest himself course floor outside our just."
+        },
+        {
+            "id": "dcdc22a0-4e79-4f83-b454-02501d938bfe",
+            "artist_name": "middle",
+            "home_carousel": [
+                {
+                    "id": "a7ff051b-d79f-4316-9d9c-0c01381db150",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "d385eedf-6ad6-49a4-aff9-ab5a9a2fa4f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "9641b0d7-771d-4344-8f60-443fea29a471",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "c6f187ab-7c6e-486e-ba69-ec366b435b2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "cacbfda9-9ebf-443a-b019-74686abe253c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                }
+            ],
+            "art_caption": "weight",
+            "art_description": "Carry environment book agreement woman trial job. Development southern save give play."
+        },
+        {
+            "id": "0e09751d-d5a6-4112-b976-c4696ebaa1cd",
+            "artist_name": "scientist",
+            "home_carousel": [
+                {
+                    "id": "8915c283-3b44-45c3-8e0a-4202332518c2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "3b94496c-6c33-4381-a6fe-968f2a2b872d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "94e77ea4-7d4b-40ba-b607-8a7f8a491e3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/17.png"
+                },
+                {
+                    "id": "761f64fb-1ba0-4e9a-b6ae-42e823dc22cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "be30ea78-7e23-40c1-9015-71a2d07c4316",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                }
+            ],
+            "art_caption": "deal",
+            "art_description": "Money require than carry left answer expect. Bar simply simple game."
+        }
+    ]
+}
